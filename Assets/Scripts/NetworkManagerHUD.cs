0b7662e97c8a10ad30f7b3d1284eb6112e96b994
Андрей Щербинин < >
2017-02-07 08:21:50 +0700

    some works on ui network manager

--- Assets/Scripts/NetworkManagerHUD.cs
+++ Assets/Scripts/NetworkManagerHUD.cs
@@ -13,6 +13,7 @@ namespace Gobzers
 	public class NetworkManagerHUD : MonoBehaviour
 	{
 		public NetworkManager manager;
+        public GameObject tbAdress;
 		[SerializeField] public bool showGUI = true;
 		[SerializeField] public int offsetX;
 		[SerializeField] public int offsetY;
@@ -76,75 +77,84 @@ namespace Gobzers
 			}
 		}
 
-		void OnGUI()
-		{
-			if (!showGUI)
-				return;
+        public void OnClickStartClient()
+        {
+            if (!NetworkClient.active && !NetworkServer.active && manager.matchMaker == null)
+            {
+                manager.StartClient();
+                manager.networkAddress = tbAdress.GetComponent("Text") as UnityEngine.UI.Text;
+            }
+        }
 
-			int xpos = 10 + offsetX;
-			int ypos = 40 + offsetY;
-			int spacing = 24;
+  //      void OnGUI()
+		//{
+		//	if (!showGUI)
+		//		return;
 
-			if (!NetworkClient.active && !NetworkServer.active && manager.matchMaker == null)
-			{
-				if (GUI.Button(new Rect(xpos, ypos, 200, 20), "LAN Host(H)"))
-				{
-					manager.StartHost();
-				}
-				ypos += spacing;
+		//	int xpos = 10 + offsetX;
+		//	int ypos = 40 + offsetY;
+		//	int spacing = 24;
 
-				if (GUI.Button(new Rect(xpos, ypos, 105, 20), "LAN Client(C)"))
-				{
-					manager.StartClient();
-				}
-				manager.networkAddress = GUI.TextField(new Rect(xpos + 100, ypos, 95, 20), manager.networkAddress);
-				ypos += spacing;
+		//	if (!NetworkClient.active && !NetworkServer.active && manager.matchMaker == null)
+		//	{
+		//		if (GUI.Button(new Rect(xpos, ypos, 200, 20), "LAN Host(H)"))
+		//		{
+		//			manager.StartHost();
+		//		}
+		//		ypos += spacing;
 
-				if (GUI.Button(new Rect(xpos, ypos, 200, 20), "LAN Server Only(S)"))
-				{
-					manager.StartServer();
-				}
-				ypos += spacing;
-			}
-			else
-			{
-				if (NetworkServer.active)
-				{
-					GUI.Label(new Rect(xpos, ypos, 300, 20), "Server: port=" + manager.networkPort);
-					ypos += spacing;
-				}
-				if (NetworkClient.active)
-				{
-					GUI.Label(new Rect(xpos, ypos, 300, 20), "Client: address=" + manager.networkAddress + " port=" + manager.networkPort);
-					ypos += spacing;
-				}
-			}
+		//		if (GUI.Button(new Rect(xpos, ypos, 105, 20), "LAN Client(C)"))
+		//		{
+		//			manager.StartClient();
+		//		}
+		//		manager.networkAddress = GUI.TextField(new Rect(xpos + 100, ypos, 95, 20), manager.networkAddress);
+		//		ypos += spacing;
 
-			if (NetworkClient.active && !ClientScene.ready)
-			{
-				if (GUI.Button(new Rect(xpos, ypos, 200, 20), "Client Ready"))
-				{
-					ClientScene.Ready(manager.client.connection);
+		//		if (GUI.Button(new Rect(xpos, ypos, 200, 20), "LAN Server Only(S)"))
+		//		{
+		//			manager.StartServer();
+		//		}
+		//		ypos += spacing;
+		//	}
+		//	else
+		//	{
+		//		if (NetworkServer.active)
+		//		{
+		//			GUI.Label(new Rect(xpos, ypos, 300, 20), "Server: port=" + manager.networkPort);
+		//			ypos += spacing;
+		//		}
+		//		if (NetworkClient.active)
+		//		{
+		//			GUI.Label(new Rect(xpos, ypos, 300, 20), "Client: address=" + manager.networkAddress + " port=" + manager.networkPort);
+		//			ypos += spacing;
+		//		}
+		//	}
+
+		//	if (NetworkClient.active && !ClientScene.ready)
+		//	{
+		//		if (GUI.Button(new Rect(xpos, ypos, 200, 20), "Client Ready"))
+		//		{
+		//			ClientScene.Ready(manager.client.connection);
 				
-					if (ClientScene.localPlayers.Count == 0)
-					{
-						ClientScene.AddPlayer(0);
-					}
-				}
-				ypos += spacing;
-			}
+		//			if (ClientScene.localPlayers.Count == 0)
+		//			{
+		//				ClientScene.AddPlayer(0);
+		//			}
+		//		}
+		//		ypos += spacing;
+		//	}
 
-			if (NetworkServer.active || NetworkClient.active)
-			{
-				if (GUI.Button(new Rect(xpos, ypos, 200, 20), "Stop (X)"))
-				{
-					manager.StopHost();
-				}
-				ypos += spacing;
-			}
+		//	if (NetworkServer.active || NetworkClient.active)
+		//	{
+		//		if (GUI.Button(new Rect(xpos, ypos, 200, 20), "Stop (X)"))
+		//		{
+		//			manager.StopHost();
+		//		}
+		//		ypos += spacing;
+		//	}
 
 
-		}
+		//}
 	}
 };
 #endif //ENABLE_UNET

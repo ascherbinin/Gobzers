7861756b1e2cc8fc4d78fe02048e3fbaa2c4e9db
AScherbinin <ascherbinin@live.ru>
2017-02-14 15:43:47 +0700

    add reload and two type of fire, single and rapid

--- Assets/Scripts/PlatformerCharacter2D.cs
+++ Assets/Scripts/PlatformerCharacter2D.cs
@@ -11,17 +11,19 @@ namespace Gobzers
         [Range(0, 1)] [SerializeField] private float m_CrouchSpeed = .36f;  // Amount of maxSpeed applied to crouching movement. 1 = 100%
         [SerializeField] private bool m_AirControl = false;                 // Whether or not a player can steer while jumping;
         [SerializeField] private LayerMask m_WhatIsGround;                  // A mask determining what is ground to the character
+		[SerializeField] private Health _health;
+		[SerializeField] private bool m_Grounded; 
 
         private Transform m_GroundCheck;    // A position marking where to check if the player is grounded.
         const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
-        private bool m_Grounded;            // Whether or not the player is grounded.
+       // Whether or not the player is grounded.
         private Transform m_CeilingCheck;   // A position marking where to check for ceilings
         const float k_CeilingRadius = .01f; // Radius of the overlap circle to determine if the player can stand up
         private Animator m_Anim;            // Reference to the player's animator component.
         private Rigidbody2D m_Rigidbody2D;
         private PlayerPosSync syncPos;
         private bool m_FacingRight = true;  // For determining which way the player is currently facing.
-
+		private bool fallDamage = false;
 		public Camera playerCamera;
         
 		private void Awake()
@@ -32,6 +34,7 @@ namespace Gobzers
             m_Anim = GetComponent<Animator>();
             m_Rigidbody2D = GetComponent<Rigidbody2D>();
             syncPos = GetComponent<PlayerPosSync>();
+			_health = GetComponent<Health> ();
         }
 
 
@@ -59,6 +62,21 @@ namespace Gobzers
 			if ((mousePos.x > transform.position.x && !m_FacingRight) || (mousePos.x < transform.position.x && m_FacingRight)) {
 				m_FacingRight = !m_FacingRight;
 				syncPos.CmdFlipSprite(m_FacingRight);
+			}
+
+			if (!m_Grounded) {
+				float currentSpeed = Mathf.Abs (m_Rigidbody2D.velocity.y);
+				Debug.Log (currentSpeed);
+				if (currentSpeed > 25f) {
+					fallDamage = true;
+					Debug.Log ("FALL");
+				} else
+					fallDamage = false;
+			}
+
+			if (m_Grounded && fallDamage) {
+				_health.TakeDamage (15);
+				fallDamage = false;
 			}
         }
 
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -13,6 +13,9 @@ namespace Gobzers
 
 		[SerializeField]
 		private Camera _curCamera;
+		private float _fireRate = 0.5f;
+		private float _lastFireTime;
+		private int _playerBullet = 6;
 		// Use this for initialization
 		void Start ()
 		{
@@ -37,23 +40,28 @@ namespace Gobzers
 			{
 				return;
 			}
+
 	        //Debug.Log("Update");
-			if (Input.GetButtonDown("Fire1"))
+			if (Input.GetButtonDown("Fire1") &&
+				Time.time > _fireRate + _lastFireTime &&
+				_playerBullet != 0)
 			{
-				var mousePos = Input.mousePosition;
-				mousePos.z = 5.23F; //The distance between the camera and object
-				var spawnPos = _curCamera.WorldToScreenPoint(bulletSpawn.position);
-				mousePos.x = mousePos.x - spawnPos.x;
-				mousePos.y = mousePos.y - spawnPos.y;
-				var angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
-				bulletSpawn.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+				SingleShot ();
+				_lastFireTime = Time.time;
+				_playerBullet--;
+			}
 
-				Vector3 worldMousePos = _curCamera.ScreenToWorldPoint(Input.mousePosition);
+			if (Input.GetButton ("Fire2") &&
+			    Time.time > _fireRate / 3 + _lastFireTime &&
+			    _playerBullet != 0) {
+				RapidFire ();
+				_lastFireTime = Time.time;
+				_playerBullet--;
+			}
 
-				Vector2 direction = (Vector2)((worldMousePos - transform.position));
-				direction.Normalize ();
-
-				CmdFire(bulletSpawn.position + (Vector3)(direction * 0.5f), Quaternion.Euler(new Vector3(0, 0, angle)), direction * 50);
+			if (Input.GetKeyDown (KeyCode.R)) {
+				Debug.Log("Reload");
+				_playerBullet = 6;
 			}
 		}
 
@@ -75,6 +83,38 @@ namespace Gobzers
 
 			// Destroy the bullet after 2 seconds
 			Destroy(bullet, 2.0f);
+		}
+
+		void RapidFire()
+		{
+			Vector2 direction = new Vector2 ();
+			float angle;
+			CalculateShotDirection (out direction, out angle);
+			CmdFire(bulletSpawn.position + (Vector3)(direction * 0.5f), Quaternion.Euler(new Vector3(0, 0, angle)), direction * 50);
+		}
+
+		void SingleShot ()
+		{
+			Vector2 direction = new Vector2 ();
+			float angle;
+			CalculateShotDirection (out direction, out angle);
+			CmdFire(bulletSpawn.position + (Vector3)(direction * 0.5f), Quaternion.Euler(new Vector3(0, 0, angle)), direction * 50);
+		}
+
+		void CalculateShotDirection (out Vector2 direction, out float angle)
+		{
+			var mousePos = Input.mousePosition;
+			mousePos.z = 5.23F; //The distance between the camera and object
+			var spawnPos = _curCamera.WorldToScreenPoint(bulletSpawn.position);
+			mousePos.x = mousePos.x - spawnPos.x;
+			mousePos.y = mousePos.y - spawnPos.y;
+			angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+			bulletSpawn.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+
+			Vector3 worldMousePos = _curCamera.ScreenToWorldPoint(Input.mousePosition);
+
+			direction = (Vector2)((worldMousePos - transform.position));
+			direction.Normalize ();
 		}
 	}
 }
